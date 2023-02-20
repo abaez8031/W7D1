b46c317de8fb67a2e568ebe77fa303cdc8eb7da1
Milner Chen <milnerchen0@gmail.com>
2023-02-20 15:44:55 -0500

    third commit

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,14 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  attr_reader :password
+  before_validation :ensure_session_token
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
-  validates :password, length: {minimum: 6}, allow: nil
+  validates :password, length: {minimum: 6}, allow_nil: true
   # before_validation
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
   
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
   end
+
+  def is_password?(password)
+    bcrypt_obj = BCrypt::Password.new(self.password_digest)
+    bcrypt_obj.is_password?(password)
+  end
   
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
   
   private
 
@@ -18,9 +50,10 @@ class User < ApplicationRecord
 
   def generate_unique_session_token
     token = SecureRandom::urlsafe_base64
-    while User.exist?(session_token: token)
+    while User.exists?(session_token: token)
       token = SecureRandom::urlsafe_base64
     end
+    token
   end
 
 end
